22effa90bf2a35e0960e4ea0bc7c2e3fe22d88a8
Alexander-work <bahmetev@dev.rus>
2021-03-22 12:34:24 +0300

    Slider 1

--- js/app.js
+++ js/app.js
@@ -71,6 +71,90 @@ function headerScroll() {
             });
         }
 
-})
+
+
+
+/*     Modal     ======================================================================= */
+
+
+$('[data-modal]').on('click', function(event){
+    event.preventDefault();
+    let modal = $(this).data('modal');
+
+    $('body').addClass('no-scroll');
+    $(modal).addClass('show');
+
+    setTimeout(function() {
+        $(modal).find('.modal__content').css({
+            transform: 'scale(1)',
+            opacity: '1'
+        });
+    }, 200);
+});
+
+
+/* Закрытие модального окна по Крестику и Фону */
+
+$('[data-modal-close]').on('click', function(event) {
+    event.preventDefault();
+    let modal = $(this).parents('.modal');
+
+    modalClose(modal);
+});
+
+/* При клике в любую область закрывается модальное окно */
+$('.modal').on('click', function() {
+    let modal = $(this);
+   
+
+    modalClose(modal);
+});
+
+/*  отменяем клик по контекту модалки, что бы она не закрывалась  */
+$('.modal__content').on('click', function(event) {
+    event.stopPropagation();
+});
+
+
+function modalClose(modal) {
+    
+    modal.find('.modal__content').css({
+        transform: 'scale(0.5)',
+        opacity: '0'
+    });
+
+    setTimeout(function() {
+        $('body').removeClass('no-scroll');
+        modal.removeClass('show');
+    }, 200);
+}
+
+
+
+
+/*   Slider Slick ------------------------------------------------ */
+
+let introSlider = $("#introSlider");
+
+introSlider.slick({
+    infinity: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    speed: 2500,
+});
+
+$('#introSliderPrev').on('click', function() {
+    introSlider.slick('slickPrev')
+});
+
+$('#introSliderNext').on('click', function() {
+    introSlider.slick('slickNext')
+});
+
+});
 
 
