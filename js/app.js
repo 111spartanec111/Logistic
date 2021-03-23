fbaf363b5fcf6e1ead6c29285ab24ba30bf13f93
Alexander-work <bahmetev@dev.rus>
2021-03-23 13:11:00 +0300

    animation +

--- js/app.js
+++ js/app.js
@@ -1,5 +1,26 @@
 $(function(){
 
+    /*  Navigation navToggle on mobile version  =============================== */
+    let navToggle = $('#navToggle');
+    let nav = $('#nav');
+
+    navToggle.on('click', function(event) {
+        event.preventDefault();
+
+        $("body").toggleClass('show-nav');
+        $(this).toggleClass('active');
+        nav.toggleClass('show');
+
+    });
+
+    $(window).on("resize", function(){
+        $("body").removeClass('show-nav');
+        navToggle.removeClass('active');
+        nav.removeClass('show');
+    });
+
+
+
     let intro = $("#intro");
     let header = $("#header");
     let introH = intro.innerHeight();
@@ -35,6 +56,10 @@ function headerScroll() {
         event.preventDefault();
         let scrollEl = $(this).data("scroll");
         let scrollElpos = $(scrollEl).offset().top;
+
+        $("body").removeClass('show-nav');
+        navToggle.removeClass('active');
+        nav.removeClass('show');
 
         $("html, body").animate({
             scrollTop: scrollElpos - headerH  /* Или -70 пикселей */
@@ -171,6 +196,30 @@ reviewsSlider.slick({
 });
 
 
+
+/* ANIMATION AOS  aos.js    ====================================== */
+AOS.init({
+    // Global settings:
+    disable: 'mobile', // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+    startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+    initClassName: 'aos-init', // class applied after initialization
+    animatedClassName: 'aos-animate', // class applied on animation
+    useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+    disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+    debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+    throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+    
+  
+    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+    offset: 80, // offset (in px) from the original trigger point
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 800, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    once: false, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+    anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+  
+  });
 
 });
 
